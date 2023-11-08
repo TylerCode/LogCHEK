f174fef28822e0244d21289ef37cb6f487c89204
Tyler C <18288340+TylerCode@users.noreply.github.com>
2023-11-07 18:04:53 -0700

    Minor formatting changes

--- src/build_windows.ps1
+++ src/build_windows.ps1
@@ -5,7 +5,7 @@ go build -o LogCHEK.exe cli.go
 ./rcedit.exe LogCHEK-GUI.exe --set-file-version "0.3.0.0"
 ./rcedit.exe LogCHEK-GUI.exe --set-product-version "0.3.0.0"
 ./rcedit.exe LogCHEK-GUI.exe --set-icon "logo.ico"
-./rcedit.exe LogCHEK.exe --set-version-string "FileDescription" "Log file checker."
+./rcedit.exe LogCHEK.exe --set-version-string "FileDescription" "Log file checker CLI."
 ./rcedit.exe LogCHEK.exe --set-version-string "ProductName" "LogCHEK"
 ./rcedit.exe LogCHEK.exe --set-file-version "0.3.0.0"
 ./rcedit.exe LogCHEK.exe --set-product-version "0.3.0.0"
