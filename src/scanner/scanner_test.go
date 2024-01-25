3730f754496dd1c40ede85671a92471b2e1d55bb
Tyler C <18288340+TylerCode@users.noreply.github.com>
2024-01-24 19:34:37 -0700

    Updated the test to set more traps for myself

--- src/scanner/scanner_test.go
+++ src/scanner/scanner_test.go
@@ -22,9 +22,9 @@ func TestScanLogs(t *testing.T) {
 		name    string
 		content string
 	}{
-		{"log1.txt", "2024-01-11T20:55:42+00:00 INFO: Nothing to see here"},
-		{"log2.txt", "2024-01-11T20:55:49+00:00 ErRoR: EVERYTHING IS ON FIRE"},
-		{"log3.txt", "2024-01-11T20:55:56+00:00 INFO: Process run with 0 errors."},
+		{"log1.txt", "2024-01-11T20:55:42+52:33 INFO: Nothing to see here"},
+		{"log2.txt", "2024-01-11T20:55:49+31:00 ErRoR: EVERYTHING IS ON FIRE"},
+		{"log3.txt", "2024-01-11T20:55:56+51:05 INFO: Process run with 0 errors."},
 	}
 
 	for _, lf := range logFiles {
